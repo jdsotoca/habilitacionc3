a23a78805a5ea01890bcfce1578271cafab1133c
duqc05 <andreslabi62@gmail.com>
2022-10-22 19:54:01 -0500

    creacion de estructura

---
+++ src/main/java/com/example/habilitacionc3/Habilitacionc3Application.java
@@ -0,0 +1,13 @@
+package com.example.habilitacionc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Habilitacionc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Habilitacionc3Application.class, args);
+	}
+
+}
